2b5b42e6dc05022f3d143265a8c7d31a2e98713b
John <jaboyak@gmail.com>
2019-08-21 11:27:58 -0700

    updated dashboard, bill pay

--- src/store/auth/constants.js
+++ src/store/auth/constants.js
@@ -3,3 +3,5 @@ export const USER_LOGIN_SUCCESS = 'USER_LOGIN_SUCCESS'
 export const USER_LOGIN_FAILED = 'USER_LOGIN_FAILED'
 
 export const USER_LOGOUT = 'USER_LOGOUT'
+
+export const BASE_URL = 'http://localhost:8082/api/users'
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
+import authReducer from './auth/reducer'
 
 // import reducers...
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({
+  auth: authReducer
+})
 
 const middleware = [thunk, logger]
 
